5a1b3b267a32798c769d6dacf2c3a1e7213adb2c
advecchia <advecchia@inf.ufrgs.br>
2013-08-04 17:40:32 -0300

    Initial files

---
+++ web.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var express = require('express');
+var app = express.createServer(express.logger());
+var buf = fs.readFileSync('index.html','utf-8')
+
+app.get('/', function(request, response) {
+  response.send(buf);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
